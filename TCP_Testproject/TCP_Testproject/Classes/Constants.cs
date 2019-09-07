acb021e113d42e348ef6ac4e98f4d32456fcb188
Lukas Kolbe <thehammerlukas.orginal@gmail.com>
2019-09-07 23:07:49 +0200

    Revert "Revert "Add""
    This reverts commit bdf32b907c7eb07a864f9dcbba047e0c657dda1c.

---
+++ TCP_Testproject/TCP_Testproject/Classes/Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Testproject.Classes
+{
+    class Constants
+    {
+        // Text alignment constants
+        public const string alignmentLeft = "alignLeft";
+        public const string alignmentCenter = "alignCenter";
+        public const string alignmentRight = "alignRight";
+
+        // Program state constants
+        public enum ProgramState
+        {
+            initializing,
+            initialized,
+            connecting,
+            connected,
+            exit
+        };
+    }
+}
