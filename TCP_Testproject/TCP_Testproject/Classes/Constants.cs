f8dfb5999a5dc79f5b5cef2719e0e38f9bccba85
Lukas Kolbe <thehammerlukas.orginal@gmail.com>
2019-09-26 20:18:59 +0200

    Started implementing a /notification chat command

--- TCP_Testproject/TCP_Testproject/Classes/Constants.cs
+++ TCP_Testproject/TCP_Testproject/Classes/Constants.cs
@@ -8,19 +8,12 @@ namespace TCP_Testproject.Classes
 {
     class Constants
     {
-        // Chat command constants
-        public const string chatCmdHelp = "/help";
-        public const string chatCmdBcBlack = "/bcblack";
-        public const string chatCmdBcWhite = "/bcwhite";
-        public const string chatCmdClear = "/clear";
-        public const string chatCmdCls = "/cls";
-        public const string chatCmdClearAll = "/clearall";
-        public const string chatCmdClsAll = "/clsall";
+        // Delimiter constants
+        public const string delimUsername = ":#USERNAME:";
+        public const string delimMsgData = ":#MSGDATA:";
 
-        // Text alignment constants
-        public const string alignmentLeft = "alignLeft";
-        public const string alignmentCenter = "alignCenter";
-        public const string alignmentRight = "alignRight";
+        // Name constants
+        public const string serverUsername = "<Server>";
 
         // Program state constants
         public enum ProgramState
@@ -34,11 +27,23 @@ namespace TCP_Testproject.Classes
             exit
         };
 
-        // Delimiter constants
-        public const string delimUsername = ":#USERNAME:";
-        public const string delimMsgData = ":#MSGDATA:";
+        // Text alignment constants
+        public const string alignmentLeft = "alignLeft";
+        public const string alignmentCenter = "alignCenter";
+        public const string alignmentRight = "alignRight";
 
-        // Special constants
-        public const string serverUsername = "<Server>";
+        // Chat command constants
+        public const string chatCmdHelp = "/help";
+        public const string chatCmdCommandHelp = "?";
+        public const string chatCmdBcBlack = "/bcblack";
+        public const string chatCmdBcWhite = "/bcwhite";
+        public const string chatCmdClear = "/clear";
+        public const string chatCmdCls = "/cls";
+        public const string chatCmdClearAll = "/clearall";
+        public const string chatCmdClsAll = "/clsall";
+        public const string chatCmdNotificationBase = "/notification";
+        public const string chatCmdNotificationSound = "-s";
+        public const string chatCmdNotificationVisual = "-v";
+        
     }
 }
